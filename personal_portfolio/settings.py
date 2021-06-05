b750adae28887969a03f50744dca51fd0e24f7a8
anjali <ani2242020@gmail.com>
2021-06-05 21:55:36 +0530

    all set for now

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/rabbit/'
+MEDIA_URL = '/media/'
 #MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
